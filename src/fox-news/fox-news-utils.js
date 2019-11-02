f992c4b726e8f77019381f7f174c64fc2d963ab1
jsasz <JustenRickert@gmail.com>
2019-11-02 03:35:20 -0500

    Start migrating the fox-news code

--- src/fox-news/fox-news-utils.js
+++ src/fox-news/fox-news-utils.js
@@ -1,5 +1,39 @@
-const isFoxVideoArticle = ({ href }) => /^https:\/\/video/.test(href);
+const isFoxVideoArticle = ({ href }) => /^https:\/\/video/.test(href)
+
+const isFox = ({ href }) => /^https:\/\/www.foxnews\.com/.test(href)
+
+const isFoxPoliticsArticleHref = ({ href }) =>
+  /^https:\/\/www.foxnews\.com\/politics\/.*/.test(href)
+
+const isFoxUsSectionHref = ({ href }) =>
+  /^https:\/\/www.foxnews\.com\/us\/?$/.test(href)
+
+const isFoxHealthArticleHref = ({ href }) =>
+  /^https:\/\/www.foxnews\.com\/health\/.*\/?$/.test(href)
+
+const isFoxLifestyleArticleHref = ({ href }) =>
+  /^https:\/\/www.foxnews\.com\/lifestyle\/[\w\-]+\/?$/.test(href)
+
+const isFoxOpinionArticleHref = ({ href }) =>
+  /^https:\/\/www.foxnews\.com\/opinion\/[\w\-]+\/?$/.test(href)
+
+const isFoxMediaArticleHref = ({ href }) =>
+  /^https:\/\/www.foxnews\.com\/media\/[\w\-]+\/?$/.test(href)
+
+const isFoxEntertainmentArticleHref = ({ href }) =>
+  /^https:\/\/www.foxnews\.com\/entertainment\/[\w\-]+\/?$/.test(href)
+
+const isFoxUsArticleHref = ({ href }) =>
+  /^https:\/\/www.foxnews\.com\/(category\/)?us\/[\w\-]+\/?$/.test(href)
 
 module.exports = {
-  isFoxVideoArticle
-};
+  isFoxVideoArticle,
+  isFox,
+  isFoxPoliticsArticleHref,
+  isFoxOpinionArticleHref,
+  isFoxMediaArticleHref,
+  isFoxEntertainmentArticleHref,
+  isFoxHealthArticleHref,
+  isFoxUsSectionHref,
+  isFoxUsArticleHref,
+}
--- src/fox-news/fox-news.js
+++ src/fox-news/fox-news.js
@@ -1,105 +1,111 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer')
 
-const { makeArticlesWithoutContentSelector } = require("../store/selectors");
+const { makeArticlesWithoutContentSelector } = require('../store/selectors')
+const { fox } = require('../store/actions')
+const { store, saveStore } = require('../store/index')
+
+const { FOX } = require('../constant')
 const {
-  makeAddArticlesAction,
-  makeUpdateArticleNormalContentAction,
-  makeUpdateArticleVideoContentAction
-} = require("../store/actions");
-const { store, saveStore } = require("../store/index");
+  and,
+  zip,
+  partitionGroups,
+  sequentiallyMap,
+  sample,
+  range,
+} = require('../utils')
 
-const { FOX_NEWS } = require("../constant");
-const { zip, sample, range } = require("../utils");
+const {
+  isFoxVideoArticle,
+  isFox,
+  isFoxPoliticsArticleHref,
+  isFoxMediaArticleHref,
+  isFoxEntertainmentArticleHref,
+  isFoxOpinionArticleHref,
+  isFoxHealthArticleHref,
+  isFoxUsSectionHref,
+  isFoxUsArticleHref,
+} = require('./fox-news-utils')
 
-const { isFoxVideoArticle } = require("./fox-news-utils");
-
-const addArticles = makeAddArticlesAction(FOX_NEWS);
-const updateArticleNormalContent = makeUpdateArticleNormalContentAction(
-  FOX_NEWS
-);
-const updateArticleVideoContent = makeUpdateArticleVideoContentAction(FOX_NEWS);
-const articlesWithoutContent = makeArticlesWithoutContentSelector(FOX_NEWS);
-
-const FOX_NEWS_URL = "https://www.foxnews.com/";
+const FOX_NEWS_URL = 'https://www.foxnews.com/'
 
 const articleParagraphs = async page => {
-  return page.$$eval(".article-body > p", articles =>
+  return page.$$eval('.article-body > p', articles =>
     Array.from(articles)
       .filter(article => {
         // These are ads that link to fox news articles unrelated to the current
         // article. They seem to be generated with a random order, which is
         // annoying...
-        return ![["strong", "a"], ["a", "strong"], ["u", "strong", "a"]].some(
-          selectorOrder => article.querySelector(selectorOrder.join(" "))
-        );
+        return ![['strong', 'a'], ['a', 'strong'], ['u', 'strong', 'a']].some(
+          selectorOrder => article.querySelector(selectorOrder.join(' '))
+        )
       })
       .map(article => ({
-        innerHTML: article.innerHTML
+        innerHTML: article.innerHTML,
       }))
-  );
-};
+  )
+}
 
 const allDOMContent = (page, selector) =>
   page.$$eval(selector, tags =>
     Array.from(tags).map(tag => ({
       href: tag.href,
-      innerHTML: tag.innerHTML
+      innerHTML: tag.innerHTML,
     }))
-  );
+  )
 
 const discoverAllHomepageArticles = async page => {
-  await page.goto(FOX_NEWS_URL);
-  await allDOMContent(page, "h2.title a")
+  await page.goto(FOX_NEWS_URL)
+  await allDOMContent(page, 'h2.title a')
     .then(articles => {
       store.dispatch(
         addArticles(
           articles.map(article => ({
             ...article,
-            title: article.innerHTML.trim()
+            title: article.innerHTML.trim(),
           }))
         )
-      );
+      )
     })
-    .catch(console.error);
-};
+    .catch(console.error)
+}
 
 const articlePageContent = async page => {
   const embeddedTwitterMedia = await allDOMContent(
     page,
-    ".article-body .embed-media.twitter"
-  );
+    '.article-body .embed-media.twitter'
+  )
   const authors = await allDOMContent(
     page,
     '.author-byline a[href^="/person/"]'
-  );
-  const videoContainer = await page.$(".video-container");
-  const ps = await articleParagraphs(page);
+  )
+  const videoContainer = await page.$('.video-container')
+  const ps = await articleParagraphs(page)
   const timestamp = await page
-    .$eval("time", time => ({
+    .$eval('time', time => ({
       scrapeDate: Date(),
-      relativeDate: time.innerHTML.trim()
+      relativeDate: time.innerHTML.trim(),
     }))
-    .catch(() => ({ scrapeDate: Date(), relativeDate: null }));
-  const images = await page.$$eval(".image-ct.inline", imageSections =>
+    .catch(() => ({ scrapeDate: Date(), relativeDate: null }))
+  const images = await page.$$eval('.image-ct.inline', imageSections =>
     Promise.all(
       imageSections.map(async section => {
-        const img = section.querySelector("img");
-        const p = section.querySelector(".caption p");
+        const img = section.querySelector('img')
+        const p = section.querySelector('.caption p')
         return {
           imageSrc: img ? img.src : null,
           caption: p
             ? p.innerHTML
-                .replace("\n", "")
-                .replace(/ */, " ")
+                .replace('\n', '')
+                .replace(/ */, ' ')
                 .trim()
-            : null
-        };
+            : null,
+        }
       })
     )
-  );
+  )
   return {
     authors,
-    content: ps.map(p => p.innerHTML).join("\n"),
+    content: ps.map(p => p.innerHTML).join('\n'),
     hasVideo: Boolean(videoContainer),
     images,
     timestamp,
@@ -112,35 +118,89 @@ const articlePageContent = async page => {
             // `twitter.com/literally-a-random-user-name/status/<twitter-id>`
             `<div class=".embed-media-twitter">${content.innerHTML}</div>`
         )
-        .join("") || null
-  };
-};
+        .join('') || null,
+  }
+}
 
-puppeteer.launch().then(async browser => {
-  const page = await browser.newPage();
-  await discoverAllHomepageArticles(page);
-  const state = store.getState();
-  await articlesWithoutContent(state).reduce(
-    (prom, article) =>
-      prom.then(async () => {
-        if (article.visited) {
-          return;
-        } else if (isFoxVideoArticle(article)) {
-          store.dispatch(updateArticleVideoContent(article));
-        } else {
-          await page.goto(article.href);
-          const articleContent = await articlePageContent(page);
-          store.dispatch(
-            updateArticleNormalContent({
-              href: article.href,
-              title: article.title,
-              ...articleContent
-            })
-          );
-        }
-      }),
-    Promise.resolve(null)
-  );
-  saveStore(store);
-  process.exit(0);
-});
+const discover = async page => {
+  const headlines = await page
+    .$$eval('a[href]', links => links.map(l => ({ href: l.href })))
+    .then(headlines => headlines.filter(isFox))
+
+  const {
+    entertainmentArticles,
+    healthArticles,
+    mediaArticles,
+    opinionArticles,
+    politics,
+    usArticles,
+    usSections,
+
+    rest,
+  } = partitionGroups(headlines, {
+    entertainmentArticles: isFoxEntertainmentArticleHref,
+    healthArticles: isFoxHealthArticleHref,
+    mediaArticles: isFoxMediaArticleHref,
+    opinionArticles: isFoxOpinionArticleHref,
+    politics: isFoxPoliticsArticleHref,
+    usArticles: isFoxUsArticleHref,
+    usSections: isFoxUsSectionHref,
+  })
+  console.log({ rest })
+  return {
+    headlines: politics,
+    usSection: usSections[0],
+    articles: usArticles
+      .concat(healthArticles)
+      .concat(opinionArticles)
+      .concat(mediaArticles)
+      .concat(entertainmentArticles),
+  }
+}
+
+const articlesWithoutContent = state =>
+  Object.values(state[FOX]).filter(article => !article.content)
+
+const run = () => {
+  puppeteer.launch({ devtools: true }).then(async browser => {
+    const page = await browser.newPage()
+    await page.goto(FOX_NEWS_URL)
+    const { headlines, usSection, articles } = await discover(page)
+    await page.goto(usSection.href)
+    const { headlines: headlines2, articles: usArticles } = await discover(page)
+    await sequentiallyMap(articlesWithoutContent(store.getState()), article => )
+  })
+}
+
+module.exports = {
+  run,
+}
+
+// puppeteer.launch().then(async browser => {
+//   const page = await browser.newPage()
+//   await discoverAllHomepageArticles(page)
+//   const state = store.getState()
+//   await articlesWithoutContent(state).reduce(
+//     (prom, article) =>
+//       prom.then(async () => {
+//         if (article.visited) {
+//           return
+//         } else if (isFoxVideoArticle(article)) {
+//           store.dispatch(updateArticleVideoContent(article))
+//         } else {
+//           await page.goto(article.href)
+//           const articleContent = await articlePageContent(page)
+//           store.dispatch(
+//             updateArticleNormalContent({
+//               href: article.href,
+//               title: article.title,
+//               ...articleContent,
+//             })
+//           )
+//         }
+//       }),
+//     Promise.resolve(null)
+//   )
+//   saveStore(store)
+//   process.exit(0)
+// })
