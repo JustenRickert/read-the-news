188b032e5e5dd902ba55c7d0888dfa54ed8b7881
jsasz <JustenRickert@gmail.com>
2019-11-03 13:01:23 -0600

    Run prettier on nbc

--- src/nbc/nbc.js
+++ src/nbc/nbc.js
@@ -1,51 +1,53 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer')
 
 const {
   makeAddArticlesAction,
   makeUpdateArticleNormalContentAction,
-  makeUpdateArticleVideoContentAction
-} = require("../store/actions");
-const { makeArticlesWithoutContentSelector } = require("../store/selectors");
-const { store, saveStore } = require("../store/index");
-const { NBC } = require("../constant");
-const { complement, partition, or } = require("../utils");
+  makeUpdateArticleVideoContentAction,
+} = require('../store/actions')
+const { makeArticlesWithoutContentSelector } = require('../store/selectors')
+const { store, saveStore } = require('../store/index')
+const { NBC } = require('../constant')
+const { complement, partition, or } = require('../utils')
 
 const {
   isNbcTextArticleLink,
   isNbcBetterHref,
-  isNbcFeatureNbcOutHref
-} = require("./nbc-utils");
+  isNbcFeatureNbcOutHref,
+} = require('./nbc-utils')
 
-const addArticles = makeAddArticlesAction(NBC);
-const updateArticle = makeUpdateArticleNormalContentAction(NBC);
-const articlesWithoutContent = makeArticlesWithoutContentSelector(NBC);
+const addArticles = makeAddArticlesAction(NBC)
+const updateArticle = makeUpdateArticleNormalContentAction(NBC)
+const articlesWithoutContent = makeArticlesWithoutContentSelector(NBC)
 
-const NBC_URL = "https://www.nbcnews.com";
+const NBC_URL = 'https://www.nbcnews.com'
+
+const NBC_SITEMAP_URL = 'https://www.nbcnews.com/sitemap'
 
 const articleContentUpdate = async page => {
   const title = await page.$eval(
     '[data-test="article-hero__headline"]',
     title => title.innerText
-  );
+  )
   const timestamp = await page
-    .$eval("time", timestamp => timestamp.innerText)
+    .$eval('time', timestamp => timestamp.innerText)
     .then(datetime => ({
       scrapeDate: Date(),
-      datetime
-    }));
+      datetime,
+    }))
   const authors = await page.$eval(
     '[data-test="byline"]',
     byline => byline.innerText
-  );
+  )
   const shouldExcludeAdditionalTrailingParagraph = or(
     isNbcBetterHref,
     isNbcFeatureNbcOutHref
-  )(page.url());
+  )(page.url())
   const content = await page.$$eval(
-    "p",
+    'p',
     (ps, shouldExcludeAdditionalTrailingParagraph) => {
-      ps = Array.from(ps);
-      const shouldExcludeFirstParagraph = ps[0].innerText[0] === "©";
+      ps = Array.from(ps)
+      const shouldExcludeFirstParagraph = ps[0].innerText[0] === '©'
       return (
         ps
           // I really hope this works for all articles lol... The first `p` tag
@@ -62,42 +64,40 @@ const articleContentUpdate = async page => {
               // bunch of unrelated content...
               !p.querySelector('a[href="https://policies.google.com/terms"]') &&
               // This is quoted text from a paragraph captured separately
-              !p.parentNode.classList.contains("liftOut")
+              !p.parentNode.classList.contains('liftOut')
           )
           .map(p => p.innerText)
-      );
+      )
     },
     shouldExcludeAdditionalTrailingParagraph
-  );
+  )
   return {
     title,
     timestamp,
     authors,
-    content
-  };
-};
+    content,
+  }
+}
 
 puppeteer.launch({ devtools: true }).then(async browser => {
-  const page = await browser.newPage();
+  const page = await browser.newPage()
   // NBC is _sometimes_ slow... TODO(maybe) handle timeouts
-  await page.setDefaultTimeout(100e3);
-  await page.goto(NBC_URL);
+  await page.setDefaultTimeout(100e3)
+  await page.goto(NBC_URL)
   const textArticleLinks = await page
-    .$$eval("a[href]", links =>
+    .$$eval('a[href]', links =>
       links.map(l => ({ href: l.href, title: l.innerText }))
     )
-    .then(links => links.filter(isNbcTextArticleLink));
-  store.dispatch(addArticles(textArticleLinks));
+    .then(links => links.filter(isNbcTextArticleLink))
+  store.dispatch(addArticles(textArticleLinks))
   await articlesWithoutContent(store.getState()).reduce(
     (p, article) =>
       p.then(async () => {
-        await page.goto(article.href);
-        const articleContent = await articleContentUpdate(page);
-        store.dispatch(
-          updateArticle({ href: article.href, ...articleContent })
-        );
-        saveStore(store);
+        await page.goto(article.href)
+        const articleContent = await articleContentUpdate(page)
+        store.dispatch(updateArticle({ href: article.href, ...articleContent }))
+        saveStore(store)
       }),
     Promise.resolve(null)
-  );
-});
+  )
+})
--- src/nbc/nbc.test.js
+++ src/nbc/nbc.test.js
@@ -1,24 +1,24 @@
 const {
   __impl: { isNbcBusinessArticleLink },
   isNbcBetterHref,
-  isNbcFeatureNbcOutHref
-} = require("./nbc-utils");
+  isNbcFeatureNbcOutHref,
+} = require('./nbc-utils')
 
-describe("nbc utils", () => {
+describe('nbc utils', () => {
   it('should not collect certain "busines" hrefs', () => {
-    const badHrefs = ["https://www.nbcnews.com/business/consumer"];
-    expect(isNbcBusinessArticleLink({ href: badHrefs[0] })).toBeFalsy();
-  });
+    const badHrefs = ['https://www.nbcnews.com/business/consumer']
+    expect(isNbcBusinessArticleLink({ href: badHrefs[0] })).toBeFalsy()
+  })
 
   it('should know about "better" hrefs', () => {
     const exampleHref =
-      "https://www.nbcnews.com/better/lifestyle/travel-website-you-re-using-says-there-s-only-1-ncna1073066";
-    expect(isNbcBetterHref(exampleHref)).toBeTruthy();
-  });
+      'https://www.nbcnews.com/better/lifestyle/travel-website-you-re-using-says-there-s-only-1-ncna1073066'
+    expect(isNbcBetterHref(exampleHref)).toBeTruthy()
+  })
 
   it('should know about "feature/nbc-out" hrefs', () => {
     const exampleHref =
-      "https://www.nbcnews.com/feature/nbc-out/almost-30-percent-bisexual-women-trans-people-live-poverty-report-n1073501";
-    expect(isNbcFeatureNbcOutHref(exampleHref)).toBeTruthy();
-  });
-});
+      'https://www.nbcnews.com/feature/nbc-out/almost-30-percent-bisexual-women-trans-people-live-poverty-report-n1073501'
+    expect(isNbcFeatureNbcOutHref(exampleHref)).toBeTruthy()
+  })
+})
